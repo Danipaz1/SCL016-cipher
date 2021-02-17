7467c8323cbc82a1996968828987763099ee8926
Danipaz1 <dn.velasquezdevia@gmail.com>
2021-02-17 13:14:48 -0300

    version2

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,45 @@
 const cipher = {
   // ...
-};
+  encode: (n, string) => {
+    let result = "";
+    let code=0;
+    //let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    for(let i=0; i<string.length; i++){
+      
+       code = string.charCodeAt(i);
+       
+        if(code>=65 && code<=90){
+          code=(code-65+parseInt(n))%26+65;
+        }
+        if(code>=97 && code<=122){
+          code=(code-97+parseInt(n))%26+97;
+        }
+        result += String.fromCharCode(code);
+      }
+          return result.toUpperCase();
+  },
+  
+    decode: (n, string) => {
+      let result = "";
+      let code=0;
+      //let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      
+      for(let i=0; i<string.length; i++){
+        
+         code = string.charCodeAt(i);
+         
+          if(code>=65 && code<=90){
+            code=(code+65-parseInt(n))%26+65;
+          }
+          if(code>=97 && code<=122){
+            code=(code+33-parseInt(n))%26+97;
+          }
+          result += String.fromCharCode(code);
+        }
+            return result.toUpperCase();
+  }
+  }
+  
+export default cipher
 
-export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import cipher from './cipher.js';
+/*b1 = botón 1,
+  b2 = botón 2,
+  t1 = texto1,
+  t2 = texto2,*/
 
-console.log(cipher);
+//definiendo el botón 1
+let cifrar = document.getElementById("b1");
+
+//Creando función flecha anónima dentro de un evento click del botón "cifrar"  
+cifrar.addEventListener("click", () => {
+    let texto1 = document.getElementById("t1").value;
+    let desplazar = document.getElementById("n1").value;
+    let textoCodificado = cipher.encode(desplazar, texto1);
+    document.getElementById("t2").innerHTML = textoCodificado;
+});
+
+//definiendo el botón 2
+let descifrar = document.getElementById("b2");
+
+//Creando función flecha anónima dentro de un evento click del botón "descifrar"
+descifrar.addEventListener("click", () => {
+    let texto2 = document.getElementById("t2").value;
+    let desplazar = document.getElementById("n1").value;
+    let textoDecodificado = cipher.decode(desplazar, texto2);
+    
+    document.getElementById("t1").innerHTML = textoDecodificado;
+});
