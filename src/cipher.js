66bb9c702b1d72aa114bab2608a9b803c7cbe00a
Danipaz1 <dn.velasquezdevia@gmail.com>
2021-02-23 09:57:28 -0300

    versión 3

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,25 @@
 const cipher = {
   // ...
-  encode: (n, string) => {
-    let result = "";
-    let code=0;
+  encode: (n, string) => { //Función flecha para codificar n(numero) y string(cadena);
+    let result = "";//Declarando una variable de resultado;
+    let code=0;//Declarando la codificación;
     //let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
-    for(let i=0; i<string.length; i++){
+    for(let i=0; i<string.length; i++){ //bucle for para recorrer el largo de la cadena.
       
-       code = string.charCodeAt(i);
+       code = string.charCodeAt(i);//aplicando charCodeAt para asignar código ASCII a la cadena.
+                                  // Declarando la variable code. 
        
-        if(code>=65 && code<=90){
-          code=(code-65+parseInt(n))%26+65;
+        if(code>=65 && code<=90){ //Condicionando los números asignados a las letras en codigo ASCII.
+          code=(code-65+parseInt(n))%26+65; //Aplicando fórmula en el caso de que la condición se cumpla. 
         }
         if(code>=97 && code<=122){
           code=(code-97+parseInt(n))%26+97;
         }
-        result += String.fromCharCode(code);
+        result += String.fromCharCode(code); //Definiendo el resultado
+                                            // Aplicando fromCharcode para retornar la cadena en letras segun ASCII.
       }
-          return result.toUpperCase();
+          return result.toUpperCase(); // Retornando resultado en mayúsculas.  
   },
   
     decode: (n, string) => {
@@ -40,6 +42,5 @@ const cipher = {
             return result.toUpperCase();
   }
   }
-  
 export default cipher
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import cipher from './cipher.js';
+
 /*b1 = botón 1,
   b2 = botón 2,
-  t1 = texto1,
-  t2 = texto2,*/
+  t1 = texto 1, (mensaje original no cifrado),
+  t2 = texto 2, (mensaje cifrado)*/
 
 //definiendo el botón 1
 let cifrar = document.getElementById("b1");
 
-//Creando función flecha anónima dentro de un evento click del botón "cifrar"  
-cifrar.addEventListener("click", () => {
+//Creando función flecha anónima dentro de un evento click que responda al botón "cifrar"  
+cifrar.addEventListener("click", () => { 
     let texto1 = document.getElementById("t1").value;
     let desplazar = document.getElementById("n1").value;
     let textoCodificado = cipher.encode(desplazar, texto1);
@@ -18,7 +19,7 @@ cifrar.addEventListener("click", () => {
 //definiendo el botón 2
 let descifrar = document.getElementById("b2");
 
-//Creando función flecha anónima dentro de un evento click del botón "descifrar"
+//Creando función flecha anónima dentro de un evento click que responda al botón "descifrar"
 descifrar.addEventListener("click", () => {
     let texto2 = document.getElementById("t2").value;
     let desplazar = document.getElementById("n1").value;
@@ -26,3 +27,4 @@ descifrar.addEventListener("click", () => {
     
     document.getElementById("t1").innerHTML = textoDecodificado;
 });
+
